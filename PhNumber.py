49be3a3c77b60a5b360da8c805235534b506b79f
investontop <85621913+investontop@users.noreply.github.com>
2024-03-17 08:01:15 +0530

    mask ph num

--- PhNumber.py
+++ PhNumber.py
@@ -1,10 +1,10 @@
 import phonenumbers
 from phonenumbers import timezone, geocoder, carrier
 
-number = '+919677168734'
+number = '+91xxxxxxx'
 
 phone = phonenumbers.parse(number)
 time = timezone.time_zones_for_number(phone)
 carrier = carrier.name_for_number(phone, "en")
 reg=geocoder.description_for_number(phone, "en")
-print(reg)  
+print(reg)
