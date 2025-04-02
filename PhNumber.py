b6ef068ab344887094f70c4573cd76cf51cb8799
investontop <85621913+investontop@users.noreply.github.com>
2025-04-02 20:57:24 +0530

    Enhance

--- PhNumber.py
+++ PhNumber.py
@@ -1,10 +1,18 @@
 import phonenumbers
-from phonenumbers import timezone, geocoder, carrier
+from phonenumbers import timezone, geocoder, carrier, is_valid_number
 
-number = '+91xxxxxxx'
+number = '+919644466735'
 
 phone = phonenumbers.parse(number)
 time = timezone.time_zones_for_number(phone)
 carrier = carrier.name_for_number(phone, "en")
-reg=geocoder.description_for_number(phone, "en")
-print(reg)
+Country=geocoder.description_for_number(phone, "en")
+
+formatted_number = phonenumbers.format_number(phone, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+
+print('['+ Country + '] [' + formatted_number + '] [' + carrier + ']')
+
+text = "Call me at 510-748-8230 if it's before 9:30, or on 703-4800500 after 10am."
+for match in phonenumbers.PhoneNumberMatcher(text, "IN"):
+    print(match)
+    print(phonenumbers.format_number(match.number, phonenumbers.PhoneNumberFormat.E164))
